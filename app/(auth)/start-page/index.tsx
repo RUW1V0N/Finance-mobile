c2d21a84e2d3a8bfc75bced3bbcc9108166f3709
Eugeniusz <von1we98@gmai.com>
2026-03-29 23:43:05 +0200

    I added start page

--- app/(auth)/start-page/index.tsx
+++ app/(auth)/start-page/index.tsx
@@ -1,30 +1,68 @@
-import { View, Text, Button, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import logo from "@/assets/logo/logo.png"
 
-export default function Start(){
-    return(
-        <View style={style.constainer}>
-            <Image source={logo} style={style.logo}/>
-            <Button style={style.buttonStart} title="Login"/>
-            <Button style={style.buttonStart} title="Registration"/>
+export default function Start() {
+    return (
+        <View style={style.wrapper}>
+            <View style={style.container}>
+                <Image source={logo} style={style.logo} />
+                <Text style={style.nameLogo}>ZentScope</Text> 
+                <Pressable style={style.buttonAuth}>
+                    <Text style={style.textButtonAuth}>Login In</Text>
+                </Pressable>
+                <Pressable style={style.buttonAuth}>
+                    <Text style={style.textButtonAuth}>Sign Up</Text>
+                </Pressable>
+                <Pressable>
+                    <Text style={style.textButtonForgot}>Forgot Password?</Text>
+                </Pressable>
+            </View>
         </View>
     )
 }
 
 const style = StyleSheet.create({
-    constainer:{
-        flex: 1,
+    wrapper:{
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fefefe"
+    },
+    container:{
+        width: "80%",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 14,
+        backgroundColor: "#fefefe"
+    },
+    logo: {
+        width: "100%",
+        height: 200,
+        resizeMode: "contain"
+    },
+    nameLogo:{
+        textAlign: "center",
+        color: "#007edf",
+        fontWeight: "bold",
+        fontSize: 30
+    },
+    buttonAuth:{
         width: "100%",
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "white"
+        backgroundColor: "#9dbefd",
+        padding: 12,
+        borderRadius: 30  
     },
-    buttonStart:{
-        width: "60%",
-        height: 50,
+    textButtonAuth:{
+        textAlign: "center",
+        fontSize: 20,
+        fontWeight: "bold",
     },
-    logo: {
-        width: "85%",
-        height: "40%",
+    textButtonForgot:{
+        textAlign: "center",
+        fontSize: 15,
+        fontWeight: "bold",
     }
 })
